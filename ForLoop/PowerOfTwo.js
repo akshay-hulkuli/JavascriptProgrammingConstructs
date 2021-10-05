c8856704e18ec090fb18e4cb4f6b06d2a91ea77b
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-05 22:47:09 +0530

    REFACTORED: to take command line input

--- ForLoop/PowerOfTwo.js
+++ ForLoop/PowerOfTwo.js
@@ -1,4 +1,8 @@
-let power = Math.floor(Math.random() * 10);
+let power;
+
+process.argv.forEach(function (val, index, array) {
+    if(index == 2) power = val; 
+});
 console.log("the value of n:"+ power);
 for(let i =0; i<= power; i++){
     console.log(Math.pow(2,i));
